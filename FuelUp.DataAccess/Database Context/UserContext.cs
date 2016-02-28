30cb03f2f64edaebba5a3317b8298090c9f2de03
alpacamybags <jsakyle@gmail.com>
2016-02-28 15:32:38 -0500

    Made some new projects to seperate business logic and data access. Also fixed a big with form validation on register screen.

---
+++ FuelUp.DataAccess/Database Context/UserContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using FuelUp.Models;
+
+namespace FuelUp.Database_Context
+{
+    public class UserContext : DbContext
+    {
+        public UserContext() :  base("UserContext")
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
