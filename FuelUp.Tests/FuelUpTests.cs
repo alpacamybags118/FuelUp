e3be4e0c472dd3d3677fc5b58f00a1194584c0c1
Robertson <KRobertson1-@mi.corp.rockfin.com>
2017-09-04 17:10:29 -0400

    added unit tests

---
+++ FuelUp.Tests/FuelUpTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FuelUp.Tests
+{
+    [TestClass]
+    public class FuelUpTests
+    {
+        [TestMethod]
+        public void ThisIsARealTest()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void ISwearThisOneIsRealToo()
+        {
+            int sum;
+
+            sum = 1 + 1;
+
+            Assert.AreEqual(2, sum);
+        }
+    }
+}
